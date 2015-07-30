0dde7d2f0fc6e79a42846a415be3443052f141ee
ptisuice <ptisuice@gmail.com>
2015-07-30 18:59:18 +0200

    rajout d'un titre

--- index.php
+++ index.php
@@ -13,5 +13,6 @@ require_once('E:\wamp\www\PourTest\func_dossier\func_lire_dossier.php');
 
 $objD = dir('E:\wamp\www\PourTest\func_dossier');
 
+echo '<br />'.'D&eacute;monstration de la commande dir() php '.'<br />';
 readRepertoire();
 ?>
