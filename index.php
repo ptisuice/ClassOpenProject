0dac8859c91321f0f58413b3505bd63c70779a90
ptisuice <ptisuice@gmail.com>
2015-07-30 11:22:53 +0200

    index.php : je retire le pointeur et le chemin

--- index.php
+++ index.php
@@ -2,10 +2,6 @@
 
 $d = dir("E:\wamp\WWW\PourTest");
 
-
-echo "Pointeur : " . $d->handle . "<br>";
-echo "Chemin : " . $d->path . "<br>";
-
 while (false !== ($entry = $d->read())) {
    echo $entry."<br>";
    }
