c0fd3e9f820246a3ee402532b8278dc13c9bcc9d
ptisuice <ptisuice@gmail.com>
2015-07-30 11:54:39 +0200

    index.php : formatage cartouche

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <?php
-/**
+/*******************************
  * @file index.php
  *
  * @author : ptisuice@gmail.com
  *******************************
  * lit un dossier
  * read a repertorie
+ *******************************
  */
 
 require_once('E:\wamp\www\PourTest\func_dossier\func_lire_dossier.php');
